e3131c9b9c9a31d5b99c6f699735b280ee6c41ca
rahulgangrade <33577332+rahulgangrade@users.noreply.github.com>
2017-11-12 18:53:33 +0530

    commit at 6:53pm

--- FirstProgram.java
+++ FirstProgram.java
@@ -4,17 +4,7 @@ public class FirstProgram {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("First Program for GitHub to test");
-		System.out.println("First Program for GitHub to test");
-		System.out.println("First Program for GitHub to test");
-		System.out.println("First Program for GitHub to test");
-		System.out.println("First Program for GitHub to test");
-		System.out.println("First Program for GitHub to test");
-		System.out.println("First Program for GitHub to test");
-		System.out.println("First Program for GitHub to test");
-		System.out.println("First Program for GitHub to test");
-		System.out.println("First Program for GitHub to test");
-		System.out.println("First Program for GitHub to test");
-		System.out.println("First Program for GitHub to test");
+	
 	}
 
 }
